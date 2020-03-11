70408f2ae23db10b42c54a96d633309dfca30630
Axell B <56314049+AxellBoue@users.noreply.github.com>
2020-03-11 12:11:27 +0100

    cadre 3D

--- phase_1/pnj/pnj phase1.gd
+++ phase_1/pnj/pnj phase1.gd
@@ -1,12 +1,18 @@
 extends KinematicBody2D
 
+# bouge
 export var numPath = 1
 var etapes : Array
-var target
+var target_path
 var i = 0
 
 export var vitesse = 500
 var direction = Vector2(0,0)
+
+#lance pansement
+var cibles_pansement_faille1
+var cibles_pansement_faille2
+var current_faille = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,14 +20,14 @@ func _ready():
 		etapes = get_node("../pnjPath1").get_children()
 	elif numPath == 2:
 		etapes = get_node("../pnjPath2").get_children()
-	target = etapes[i]
+	target_path = etapes[i]
 	
 
 func _physics_process(delta):
-	direction = -(global_position - target.global_position).normalized()
+	direction = -(global_position - target_path.global_position).normalized()
 	move_and_slide(direction * vitesse)
 	z_index = global_position.y/3
-	if (global_position - target.global_position).length() <= 50:
+	if (global_position - target_path.global_position).length() <= 50:
 		change_target()
 
 
@@ -29,4 +35,4 @@ func change_target():
 	i += 1
 	if i >= etapes.size():
 		i = 0
-	target = etapes[i]
+	target_path = etapes[i]
